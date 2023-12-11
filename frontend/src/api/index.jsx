15d1b7973eb0f7dca879cdf8e1d9c055d3dce1e7
Vivek Thakur <vivekthakuruspc@gmail.com>
2023-12-11 23:54:33 +0530

    url update

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "/api",
+  baseURL: "https://deploy-themern-api.vercel.app/" || "/api",
 });
 
 export default api;
