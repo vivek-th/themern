397f93373bedf79098a20c853d3f93ff55c43c17
Vivek Thakur <vivekthakuruspc@gmail.com>
2023-12-11 23:32:27 +0530

    url update

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
-const baseURL = process.env.baseURL;
 
 const api = axios.create({
-  baseURL: "/api",
+  baseURL: process.env.REACT_APP_API_URL || "/api",
 });
+
 export default api;
